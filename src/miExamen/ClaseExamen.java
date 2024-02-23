f170bbebe9a8234da5920b1035410f4bc6040a9a
David <davidbv92@educastur.es>
2024-02-23 19:22:34 +0100

    Añadidos comentarios de documentacion claseExamen

--- src/miExamen/ClaseExamen.java
+++ src/miExamen/ClaseExamen.java
@@ -1,8 +1,13 @@
 package miExamen;
 
 /**
- *
+ *<h1>Entornos de desarrollo UT3</h1>
+ * <p>Esta clase sirve para realizar el examen de la UT3 del módulo Entornos de
+Desarrollo</p>
+* <a href=https://www.youtube.com/playlist?list=PLU8oAlHdN5BlyaPFiNQcV0xDqy0eR35aU>Curso de git y Github de púldoras informáticas</a>
  * @author David
+ * @version 1.0
+ * @since 23/02/2024
  */
 public class ClaseExamen {
     public static void main(String[] args) {
