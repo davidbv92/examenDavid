1071696bce5d61241178f9c23fb02726d63253da
David <davidbv92@educastur.es>
2024-02-23 19:15:43 +0100

    Añadidos los primeros ficheros al proyecto

---
+++ src/miExamen/ClaseExamen.java
@@ -0,0 +1,11 @@
+package miExamen;
+
+/**
+ *
+ * @author David
+ */
+public class ClaseExamen {
+    public static void main(String[] args) {
+        System.out.println("Estoy haciendo el examen de la UT3");
+    }
+}
